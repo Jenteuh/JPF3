f2c5fa64ccdfa17940c657b17bd3cbe2e122d17d
Jente Huysmans <jentehuysmans@hotmail.com>
2025-04-20 13:19:09 +0200

    Temperatuur

---
+++ src/be/vdab/jpf5/Temperatuur.java
@@ -0,0 +1,13 @@
+package be.vdab.jpf5;
+
+public class Temperatuur {
+
+    public static void main(String[] args) {
+        float tempCelsius = 37.0F;
+        float tempFahr = tempCelsius * 9 / 5 + 32;
+        System.out.println("Temperatuur in graden Celsius: " +
+                tempCelsius);
+        System.out.println("Temperatuur in graden Fahrenheit: " +
+                tempFahr);
+    }
+}
