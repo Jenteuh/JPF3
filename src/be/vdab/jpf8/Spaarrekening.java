049984c587ec743e829ef61fd3e28e2d9bbaf679
Maria Kremliovsky <mkremlio@ucsd.edu>
2025-05-12 16:27:20 -0500

    Class Spaarrekening + Main

---
+++ src/be/vdab/jpf8/Spaarrekening.java
@@ -0,0 +1,47 @@
+package be.vdab.jpf8;
+
+public class Spaarrekening {
+    private String rekeningNummer;
+    private double saldo;
+    private double intrest;
+    public Spaarrekening(String reknr, double intrest) {
+        rekeningNummer = reknr;
+        this.intrest = intrest;
+    }
+    public Spaarrekening(String reknr, double intrest, double saldo) {
+        this(reknr, intrest);
+        this.saldo = saldo;
+    }
+    public String getRekeningNummer(){
+        return rekeningNummer;
+    }
+    public void setRekeningNummer(String reknr) {
+        rekeningNummer = reknr;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getIntrest() {
+        return intrest;
+    }
+
+    public void setIntrest(double intrest) {
+        this.intrest = intrest;
+    }
+
+    public void storten (double bedrag) {
+        saldo += bedrag;
+    }
+    public void afhalen (double bedrag) {
+        saldo -= bedrag;
+    }
+    public void overschrijven(Spaarrekening spaarRek, double bedrag){
+        saldo -= bedrag;
+        spaarRek.storten(bedrag);
+    }
+}
