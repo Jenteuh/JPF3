2576cfb5ec23977cc5375807a21ffdb588c9d54b
Maria Kremliovsky <mkremlio@ucsd.edu>
2025-05-15 13:36:26 -0500

    DebugVoorbeeld

---
+++ src/be/vdab/jpf11/DebugVoorbeeld.java
@@ -0,0 +1,14 @@
+package be.vdab.jpf11;
+
+public class DebugVoorbeeld {
+    public static void main(String[] args) {
+        var tekst = "Ik ben een Java Developer";
+        System.out.println(plaatsAchterElkeStringEenPunt(tekst));
+    }
+    private static String plaatsAchterElkeStringEenPunt(String eenString) {
+        if (!eenString.endsWith(".")) {
+            eenString = eenString.concat(".");
+        }
+        return eenString;
+    }
+}
