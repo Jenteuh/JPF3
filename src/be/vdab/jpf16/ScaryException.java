232e4380e51fe0db89c5fc520a8bb7b1e059321d
Maria Kremliovsky <mkremlio@ucsd.edu>
2025-05-21 11:33:28 -0500

    Exception test jpf16

---
+++ src/be/vdab/jpf16/ScaryException.java
@@ -0,0 +1,8 @@
+package be.vdab.jpf16;
+
+public class ScaryException extends java.lang.Exception {
+    public ScaryException() { }
+    public ScaryException(String msg) {
+        super(msg);
+    }
+}
