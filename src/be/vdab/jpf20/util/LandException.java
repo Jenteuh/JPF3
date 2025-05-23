ab310a53edb5006e926d49b84b7b55d8ecac1a0e
Maria Kremliovsky <mkremlio@ucsd.edu>
2025-05-23 15:15:27 -0500

    exercise Land

---
+++ src/be/vdab/jpf20/util/LandException.java
@@ -0,0 +1,8 @@
+package be.vdab.jpf20.util;
+
+public class LandException extends RuntimeException {
+    public LandException() {}
+    public LandException(String message) {
+        super(message);
+    }
+}
